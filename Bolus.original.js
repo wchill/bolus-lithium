b61f19618d33a2cb789ca315ac366d4996c98152
Eric Ahn <wchill1337@gmail.com>
2015-06-30 01:43:12 -0500

    Initial commit
    Initial version of bolus-lithium (color and standard versions).

---
+++ Bolus.original.js
@@ -0,0 +1,69 @@
+function renderBattery(height, percentage, charging, low, color) {
+    var emptyBarCanvas = document.createElement("canvas"),
+        emptyBarContext = emptyBarCanvas.getContext("2d"),
+        emptyBarData,
+        fullBarCanvas = document.createElement("canvas"),
+        fullBarContext = fullBarCanvas.getContext("2d"),
+        fullBarData,
+        finalCanvas = document.createElement("canvas"),
+        finalContext = finalCanvas.getContext("2d"),
+        barWidth = height * 1.25,
+        radius = Math.round(height / 6) / 2.0,
+        totalWidth = barWidth + (charging ? Math.round(0.3 * height) + 10 : 5),
+        colorString = "rgb(" + color.join() + ")",
+        colorStringAlpha = "rgba(" + color.join() + ",0.3)",
+        barData;
+    finalCanvas.width = totalWidth;
+    finalCanvas.height = height;
+    emptyBarCanvas.width = barWidth;
+    emptyBarCanvas.height = height;
+    fullBarCanvas.width = barWidth;
+    fullBarCanvas.height = height;
+
+    function drawPath(ctx) {
+        ctx.beginPath();
+        ctx.arc(radius, height / 2, radius, 0.5 * Math.PI, 1.5 * Math.PI);
+        ctx.lineTo(barWidth - radius / 2, height / 2 - radius);
+        ctx.arc(barWidth - radius, height / 2, radius, 1.5 * Math.PI, 0.5 * Math.PI);
+        ctx.lineTo(radius / 2, height / 2 + radius);
+        ctx.clip();
+    }
+
+    drawPath(emptyBarContext);
+    emptyBarContext.fillStyle = colorStringAlpha;
+    emptyBarContext.fill();
+    if (charging) fullBarContext.fillStyle = "#4CD964";
+    else if (low) fullBarContext.fillStyle = "#FF3B30";
+    else fullBarContext.fillStyle = colorString;
+    drawPath(fullBarContext);
+    fullBarContext.fill();
+    fullBarData = fullBarContext.getImageData(0, 0, barWidth, height);
+    finalContext.putImageData(fullBarData, 0, 0);
+    barData = emptyBarContext.getImageData(barWidth * (percentage / 100), 0, barWidth, height);
+    finalContext.putImageData(barData, barWidth * (percentage / 100), 0);
+
+    if (charging) {
+        var lightningCanvas = document.createElement("canvas");
+        var lightningContext = lightningCanvas.getContext("2d");
+        var lightningBase = 0.75 * height;
+        lightningCanvas.width = Math.round(0.4 * lightningBase);
+        lightningCanvas.height = Math.round(0.7 * lightningBase);
+        lightningContext.strokeStyle = colorString;
+        lightningContext.fillStyle = colorString;
+        lightningContext.beginPath();
+        lightningContext.moveTo(Math.round(0.3 * lightningBase), 0);
+        lightningContext.lineTo(Math.round(0.2 * lightningBase), Math.round(0.3 * lightningBase));
+        lightningContext.lineTo(Math.round(0.4 * lightningBase), Math.round(0.3 * lightningBase));
+        lightningContext.lineTo(Math.round(0.1 * lightningBase), Math.round(0.7 * lightningBase));
+        lightningContext.lineTo(Math.round(0.2 * lightningBase), Math.round(0.4 * lightningBase));
+        lightningContext.lineTo(0, Math.round(0.4 * lightningBase));
+        lightningContext.closePath();
+        lightningContext.stroke();
+        lightningContext.clip();
+        lightningContext.fill();
+        var lightningData = lightningContext.getImageData(0, 0, lightningCanvas.width, lightningCanvas.height);
+        finalContext.putImageData(lightningData, barWidth + 5, height / 2 - lightningCanvas.height / 2);
+    }
+
+    return finalCanvas.toDataURL("image/png");
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bolus-lithium
-Port of the Bolus theme for Alkaline to Lithium.
+Port of the Bolus theme for Alkaline to Lithium for iOS. Color and standard versions available.
+
+Original source is in `.original.js` files.
+
+## Compilation
+
+Remove the function signature and brackets, then test using https://supermamon.github.io/Lithium/ThemeMaker.html
+
+Use http://closure-compiler.appspot.com/home (with simple settings) to minify. Make sure to:
+
+* Remove `function renderBattery` from the beginning of the script. The first character of the script should be `(` (left parenthesis).
+* Remove the semicolon `;` from the end of the script. The last character of the script should be `}` (right curly brace).
+
+## Installation
+Copy `Bolus.js` and/or `Bolus Color.js` to the directory `/Library/Lithium/` on your iOS device.
+
+Activate the desired theme in Lithium's settings.
